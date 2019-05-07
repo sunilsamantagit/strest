973e68530820f890502c4e84fe66ab4f7bf64331
sunil_samanta <sunil.bluehorse@gmail.com>
2019-05-07 11:08:28 +0530

    sss222

--- application/config/database.php
+++ application/config/database.php
@@ -77,11 +77,7 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost', 
 	'username' => 'root', 
-<<<<<<< HEAD
-	'password' => "",	
-=======
-	'password' => 'password',	
->>>>>>> 9fb3c01f02f1a626d1d17081c4b6d2f2e6bda201
+	'password' => '',	
 	'database' => '328057_smmetal', 
 	'dbdriver' => 'mysqli',
 	'dbprefix' => 'strest_',
